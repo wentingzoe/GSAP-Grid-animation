0da80fbf671abd0fc076bfe3b57c40579721d2cb
Wenting Y <ywtzoe@gmail.com>
2024-10-17 09:36:30 -0400

    Refactor FollowEye component to add mouse tracking for eye movement

--- src/components/FollowEye.tsx
+++ src/components/FollowEye.tsx
@@ -1,11 +1,58 @@
-import React, { useRef } from "react";
-// import gsap from "gsap";
+import React, { useRef, useEffect } from "react";
+import gsap from "gsap";
 
-const FollowEye: React.FC<{ backgroundColor: string }> = ({
-  backgroundColor,
-}) => {
+const FollowEye: React.FC<{
+  backgroundColor: string;
+  mousePosition?: { x: number; y: number } | null;
+}> = ({ backgroundColor, mousePosition }) => {
   const containerRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (!containerRef.current || !mousePosition) return;
 
+    const container = containerRef.current;
+    const maxTrans = 10;
+    const maxX = container.clientWidth / 2;
+    const maxY = container.clientHeight / 2;
+
+    const eyes = container.querySelectorAll<HTMLElement>(".eye");
+    const pupils = container.querySelectorAll<HTMLElement>(".eye-pupil");
+
+    eyes.forEach((eye, i) => {
+      const pupil = pupils[i];
+      const eyeRect = eye.getBoundingClientRect();
+      const r = eyeRect.width / 2;
+      const centerX = eyeRect.left + r;
+      const centerY = eyeRect.top + r;
+
+      let x = mousePosition.x - centerX;
+      let y = mousePosition.y - centerY;
+      // Limit the movement within the bounds of the eye
+      x = Math.max(-maxX, Math.min(x, maxX));
+      y = Math.max(-maxY, Math.min(y, maxY));
+
+      const xPercent = x / maxX;
+      const yPercent = y / maxY;
+
+      const scaledXPercent = xPercent * maxTrans;
+      const scaledYPercent = yPercent * maxTrans;
+
+      gsap.to(pupil, {
+        xPercent: scaledXPercent,
+        yPercent: scaledYPercent,
+        duration: 0.4,
+        // ease: "power2.out",
+        overwrite: "auto",
+      });
+
+      gsap.to(eye, {
+        xPercent: scaledXPercent * 0.4,
+        yPercent: scaledYPercent * 0.4,
+        duration: 0.4,
+        // ease: "power2.out",
+        overwrite: "auto",
+      });
+    });
+  }, [mousePosition]);
   return (
     <div className="box" ref={containerRef}>
       <div className="box-content">
